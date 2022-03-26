6459365c12a78cbf22adce78d3e3086fa97a90bc
annamoore <annavmoore@msn.com>
2022-03-26 16:11:30 -0400

    Added First file

--- src/day0/First.java
+++ src/day0/First.java
@@ -4,6 +4,7 @@ public class First {
     public static void main(String[] args) {
         System.out.println("java");
         System.out.println("Working...");
+        System.out.println("hi");
     }
     // TODO: work on this tomorrow
     // add some cool code
